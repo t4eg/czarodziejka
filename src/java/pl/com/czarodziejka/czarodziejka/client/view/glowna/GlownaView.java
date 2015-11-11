bd84c64cadcfeb83e4824087e7fe7ed0a2f75a3f
admin <admin@192.168.1.100>
2015-11-11 20:25:31 +0100

    poprawki od mamy

--- src/java/pl/com/czarodziejka/czarodziejka/client/view/glowna/GlownaView.java
+++ src/java/pl/com/czarodziejka/czarodziejka/client/view/glowna/GlownaView.java
@@ -16,11 +16,11 @@ public class GlownaView extends VerticalPanel implements CanShow {
         add(new HTML("<p style=\"text-indent: 45px;\">Zapraszamy Państwa do skorzystania z oferty naszej wypożyczalni. Posiadamy stroje karnawałowe, teatralne, na Jasełka i Haloween w rozmiarach od 80 cm dla dzieci do rozmiarów XXL dla dorosłych.</p>"
                 + "<p>Spora część kostiumów to własne wzory uwzględniające sugestie i potrzeby klientów.</p>"
                 + "<p>Prezentowane zdjęcia są częścią tego co możecie Państwo u nas wypożyczyć. Na bieżąco powiększamy naszą kolekcję, aby sprostać oczekiwaniom dzieci i dorosłych.</p>"
-                + "<p style=\"width:645px;font-size:21px; color:red;font-weight:bold;text-align:center;\">"
-                + "Istnieje możliwość rezerwacji strojów.<br/>"
+                + "<p style=\"width:100%;font-size:21px; color:red;font-weight:bold;text-align:center;margin-top:16px;margin-bottom:16px;\">"
+//                + "Istnieje możliwość rezerwacji strojów.<br/>"
                 + "Podczas karnawału nie przyjmujemy rezerwacji na stroje dziecięce.</p>"
 
-                + "<p style=\"font-size: 22px;\">Godziny pracy wypożyczalni:</p>"));
+                + "<p style=\"font-size: 21px;margin-bottom:0px;\">Godziny pracy wypożyczalni:</p>"));
         try {
             CalendarView calendar = new CalendarView();
             calendar.setStylePrimaryName("mainCalendar");
